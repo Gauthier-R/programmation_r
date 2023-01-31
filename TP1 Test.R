6b77b3379058dc7432e3888a3cae9bafd4a0cf82
Gauthier-R <123742571+Gauthier-R@users.noreply.github.com>
2023-01-31 17:21:13 +0100

    Update TP1 Test.R

--- TP1 Test.R
+++ TP1 Test.R
@@ -1,1 +1,16 @@
-Ceci eest un test
+brutToNet1 <- function(brute, type){
+  
+  if(is.character(type)){
+    calcul <- brute - (brute *7.5 / 100)
+  
+    if (type == "cadre") {
+      calcul1 <- calcul - (calcul *25 / 100)
+      print(paste("Votre salaire est de", calcul, "euros"))
+      return(calcul1)
+    }else {
+      calcul1 <- calcul - (calcul *22 / 100)
+      print(paste("Votre salaire est de", calcul, "euros"))
+      return(calcul1)
+    }}else {
+    print("ERROR : contract unknown")
+    }}
